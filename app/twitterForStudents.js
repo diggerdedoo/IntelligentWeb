7d0ce8459738c99cd694f6f5876e076efaf16731
Ryan Dignum <allapologies111@gmail.com>
2016-03-13 20:40:54 +0000

    Third commit

---
+++ app/twitterForStudents.js
@@ -0,0 +1,74 @@
+var Twit = require('twit');
+var client = new Twit({
+  consumer_key: 'aGvgbxGtSKTWdcxGf3paz90Kq',
+  consumer_secret: 'PJwjJPJFUuWuEvzyj3G2fxbfy7yvFb00mRd45rRrKtJ4Ea39rj',
+  access_token: '221331440-s4Bgw21Z9xhlGe8QuGphtKjx0ZXg4h3sEYNUERff',
+  access_token_secret: 'LlQCF7QCj5UOlli8LSU4BhAnF9ouiJliSZ7bFeOqm36p9',
+});
+
+var allData = [];
+var i = 0;
+var manUtd = getManUtdTweets();
+var london = getLondonTweets();
+var manUtdProfile = getManUtdProfile();
+console.log('Started')
+
+function handleLondonTweets(err, data){
+  if (err) {
+    console.error('Get error', err)
+  }  
+  else {
+    console.log('Get Tweets');
+    allData = allData.concat(data);
+    i += 1;
+    if (i == 3){
+      console.log(allData);
+      console.log('Finished');
+    }
+  }
+}
+
+function handleManUtdTweets(err, data){
+  if (err) {
+    console.error('Get error', err)
+  }  
+  else {
+    console.log('Get Tweets');
+    allData = allData.concat(data);
+    i += 1;
+    if (i == 3){
+      console.log(allData);
+      console.log('Finished');
+    }
+  }
+}
+
+function handleFriends(err, data){
+  if (err) {
+    console.error('Get error', err)
+  }  
+  else {
+    console.log('Get Friends');
+    i += 1;
+    if (i == 3){
+      console.log(allData);
+      console.log('Finished');
+    }
+  }
+}
+
+
+function getManUtdTweets(){
+  client.get('search/tweets', { q: 'Liverpool since:2011-11-11 lang:en', count: 100, from: 'manutd' },
+              handleManUtdTweets)
+}
+
+function getLondonTweets(){
+  client.get('search/tweets', { q: 'Drones since:2011-11-11 lang:en geocode:51.5072,0.1275,200km', count: 100, from: 'manutd' },
+              handleLondonTweets)
+}
+
+function getManUtdProfile( callback ){
+  client.get('friends/list', { screen_name: 'manutd' , count: 200 },
+             handleFriends)
+}
